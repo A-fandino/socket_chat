c89b77d96b745b490426741e38bbd82316330c67
A-fandino <arnauf8@gmail.com>
2021-06-05 20:06:59 +0200

    Sending id from server

--- server/index.js
+++ server/index.js
@@ -7,12 +7,18 @@ const PORT = 8080;
 http.listen(PORT, () => console.log(`Listening on *:${PORT}`));
 
 io.on("connection", (socket) => {
+  console.log(socket);
   console.log("new client");
   socket.on("message", (evt) => {
     console.log(evt);
     socket.broadcast.emit("message", {
+      id: socket.id,
       user: evt.User.username,
       msg: evt.msg,
     });
   });
+
+  // socket.on("typing", (user) => {
+  //   console.log(user);
+  // });
 });
